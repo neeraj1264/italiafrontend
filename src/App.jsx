442a655e9900db14f7aeb3fefe292b370ebdced5
neeraj1264 <121123848+neeraj1264@users.noreply.github.com>
2025-06-24 13:11:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,7 @@ const App = () => {
   const [baseUrl, setBaseUrl] = useState(""); // Dynamic BASE_URL
 
   const currentRoute = window.location.pathname;
-  const USERS = JSON.parse(import.meta.env.VITE_USERS || "[]");
 
-  console.log("getting data from env fie " , USERS)
   // Clear 'productsToSend' from localStorage on page reload
   useEffect(() => {
     // Check if the user is already logged in
